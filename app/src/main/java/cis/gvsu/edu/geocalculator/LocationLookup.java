ef37b597d6215a05ca0b16f6ffb10140e4688b5d
Sahana <Sahana@Nandakumarans-MacBook-Pro.local>
2017-06-13 00:53:22 -0400

    Initial commit

---
+++ app/src/main/java/cis/gvsu/edu/geocalculator/LocationLookup.java
@@ -0,0 +1,72 @@
+package cis.gvsu.edu.geocalculator;
+
+import org.joda.time.DateTime;
+import org.parceler.Parcel;
+
+
+/**
+ * Created by prabhjot on 6/8/2017.
+ */
+@Parcel
+public class LocationLookup {
+
+        double origLat;
+        double origLng;
+        double endLat;
+        double endLng;
+        String _key;
+    String timestamp;
+
+    //public LocationLookup(Double s, Double s1, Double s2, Double s3, DateTime now) {
+    //}
+
+    public LocationLookup() {
+
+    }
+
+
+    public double getOrigLat() {
+        return origLat;
+    }
+
+    public void setOrigLat(double origLat) {
+        this.origLat = origLat;
+    }
+
+    public double getOrigLng() {
+        return origLng;
+    }
+
+    public void setOrigLng(double origLng) {
+        this.origLng = origLng;
+    }
+
+    public double getEndLat() {
+        return endLat;
+    }
+
+    public void setEndLat(double endLat) {
+        this.endLat = endLat;
+    }
+
+    public double getEndLng() {
+        return endLng;
+    }
+
+    public void setEndLng(double endLng) {
+        this.endLng = endLng;
+    }
+
+    public String get_key() {
+        return _key;
+    }
+
+    public void set_key(String _key) {
+        this._key = _key;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+}
+
